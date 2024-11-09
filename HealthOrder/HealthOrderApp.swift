4c2b86c284a24ab928d120fb6fec809c921cbfc9
wrdowney <me@wrdowney.com>
2024-11-09 09:00:03 -0500

    Initial Commit

---
+++ HealthOrder/HealthOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthOrderApp.swift
+//  HealthOrder
+//
+//  Created by Will D on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthOrderTests/HealthOrderTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthOrderTests.swift
+//  HealthOrderTests
+//
+//  Created by Will D on 11/9/24.
+//
+
+import Testing
+@testable import HealthOrder
+
+struct HealthOrderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
